93a93a586f54c8d2c6e38260282d2d6e7012f335
Gingertonic <bethmschofield@gmail.com>
2018-12-24 06:05:50 +0000

    implement shuffle cards

--- merry-christmas-everyone-api/db/seeds.rb
+++ merry-christmas-everyone-api/db/seeds.rb
@@ -6,4 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Recipient.create(name: "Ian and Lisa", message: "Have a cosy Christmas!", image1: "#", image2: "#", image3: "#", image4: "#", slug: "ian-and-lisa")
+Recipient.create(name: "Ian and Lisa", message: "Have a cosy Christmas!", image1: "1", image2: "2", image3: "3", image4: "4", slug: "ian-and-lisa")
+Recipient.create(name: "Jasmine", message: "Have a cosy Christmas!", image1: "#", image2: "#", image3: "#", image4: "#", slug: "jasmine")
+Recipient.create(name: "Bernice and Dennis", message: "Have a cosy Christmas!", image1: "#", image2: "#", image3: "#", image4: "#", slug: "bernice=and-dennis")
+Recipient.create(name: "Michelle, Sam and Kian", message: "Have a cosy Christmas!", image1: "#", image2: "#", image3: "#", image4: "#", slug: "michelle")
+Recipient.create(name: "Didi and Renaldo", message: "Have a cosy Christmas!", image1: "#", image2: "#", image3: "#", image4: "#", slug: "didi-and-renaldo")
+Recipient.create(name: "Nat and Caz", message: "Have a cosy Christmas!", image1: "#", image2: "#", image3: "#", image4: "#", slug: "nat-and-caz")
+Recipient.create(name: "Sammy and Grace", message: "Have a cosy Christmas!", image1: "#", image2: "#", image3: "#", image4: "#", slug: "sammy-and-grace")
+Recipient.create(name: "Joey and In-shil", message: "Have a cosy Christmas!", image1: "#", image2: "#", image3: "#", image4: "#", slug: "joey-and-inshil")
